aa18f144f8e6ac20da08bdf5b02437c1758d4db4
EdenSolll <erg9359g.rit.edu>
2023-10-19 10:57:48 -0400

    code updates

---
+++ Lab06/store location.py
@@ -0,0 +1,18 @@
+"""
+file: insertion_sort.py
+language: python3
+author: Arthur Nunes-Harwitt
+purpose: Implementation of insertion sort algorithm
+"""
+
+
+def compute():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    pass
