2c803de91225029268a5fe44374b0bcc3cc7842f
EdenSolll <erg9359g.rit.edu>
2023-10-22 07:44:52 -0400

    uhh in lab hopefully done

--- Lab06/store location.py
+++ Lab06/store location.py
@@ -9,12 +9,25 @@ import tools
 
 
 def compute(lst):
-    sorted_list = insertion_sort(lst)
+    original_list = lst.copy()
+    sorted_list = insertion_sort.insertion_sort(lst)
+    optimal_location = tools.median(sorted_list)
+    sumdist = tools.sum_of_distances(original_list, optimal_location)
+    return optimal_location, sumdist
 
 
 def main():
-    pass
+    file = input("Enter data file:")
+    while True:
+        try:
+            read_file = open(file, "r")
+            locations_lst = tools.filereader(read_file)
+            num1, num2 = compute(locations_lst)
+            print(f"Optimum new store location: {num1}")
+            print(f"Sum of distances to the new store: {num2}")
+        except FileNotFoundError:
+            print("invalid file name or path, try again")
 
 
 if __name__ == "__main__":
-    pass
+    main()
--- Lab06/tools.py
+++ Lab06/tools.py
@@ -2,24 +2,22 @@ import re
 
 
 def filereader(input_file):
-    with open(input_file, "r") as file:
-        text = file.read()
-        numbers = re.findall("\d+", text)
-        return numbers
+    numbers = re.findall("\d+", input_file)
+    return numbers
 
 
-def distances(lst, location):
+def sum_of_distances(lst, location):
     x = 0
     for a in lst:
         x += abs(location - lst[a])
     return x
 
 
-def med(sortedlist):
+def median(sortedlist):
     if len(sortedlist) % 2:
         return sortedlist[(len(sortedlist) - 1) // 2]
     else:
         return (
             sortedlist[(len(sortedlist) - 1) // 2]
             + sortedlist[((len(sortedlist) - 1) // 2) + 1]
-        ) / 2
+        )
